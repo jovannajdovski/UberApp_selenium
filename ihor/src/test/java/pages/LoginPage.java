ed5e61e08bba306d1db72f71ea7ed210dc64c13b
serbedzijajovan <jovajoksa@gmail.com>
2023-02-06 20:50:57 +0100

    Added login test

--- ihor/src/test/java/pages/LoginPage.java
+++ ihor/src/test/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,26 +16,56 @@ public class LoginPage {
     WebElement passwordField;
     @FindBy(css = "#loginbtn")
     WebElement loginButton;
-    public LoginPage(WebDriver webDriver)
-    {
-        this.driver=webDriver;
-        PageFactory.initElements(this.driver,this);
+    @FindBy(css = ".login-error")
+    WebElement loginError;
+    @FindBy(css = ".input-error")
+    WebElement emailError;
+
+    public LoginPage(WebDriver webDriver) {
+        this.driver = webDriver;
+        PageFactory.initElements(this.driver, this);
     }
-    public void inputUsername(String username)
-    {
+
+    public void inputUsername(String username) {
+        usernameField.clear();
         usernameField.sendKeys(username);
     }
-    public void inputPassword(String password)
-    {
+
+    public void inputPassword(String password) {
+        passwordField.clear();
         passwordField.sendKeys(password);
     }
-    public void clickOnLogin()
-    {
+
+    public void clickOnLogin() {
         loginButton.click();
     }
+
+    public void clickOnEmailInput() {
+        usernameField.click();
+    }
+
+    public void inputEmailWrongFormat() {
+        usernameField.click();
+        usernameField.sendKeys(Keys.SPACE);
+        usernameField.sendKeys(Keys.RETURN);
+
+    }
+
     public boolean isOpened() {
-        WebDriverWait wait=new WebDriverWait(this.driver, 10);
+        WebDriverWait wait = new WebDriverWait(this.driver, 10);
         wait.until(ExpectedConditions.visibilityOf(loginButton));
+        return true;
+    }
+
+    public boolean errorIsShown() {
+        WebDriverWait wait = new WebDriverWait(this.driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(loginError));
+        return true;
+    }
+
+    public boolean emailErrorIsShown() {
+        WebDriverWait wait = new WebDriverWait(this.driver, 10);
+        wait.until(ExpectedConditions.visibilityOf(emailError));
         return true;
     }
 }
---
+++ ihor/src/test/java/tests/PassengerLoginTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.UnregisteredUserPage;
+
+public class PassengerLoginTest {
+    public static WebDriver chrome_driver;
+
+    private final String PASSENGER_EMAIL = "passenger@gmail.com";
+    private final String PASSENGER_PASSWORD = "NekaSifra123";
+
+    @BeforeClass
+    public void initDriver() {
+        System.setProperty("webdriver.chrome.driver", "../../chromedriver");
+        chrome_driver = new ChromeDriver();
+
+        chrome_driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void quitDriver() {
+        chrome_driver.quit();
+    }
+
+    @Test
+    public void passengerLogin() {
+        UnregisteredUserPage unregisteredUserPagePassenger = new UnregisteredUserPage(chrome_driver);
+
+        Assert.assertTrue(unregisteredUserPagePassenger.isOpened());
+
+        unregisteredUserPagePassenger.clickOnLogin();
+
+        LoginPage loginPagePassenger = new LoginPage(chrome_driver);
+        Assert.assertTrue(loginPagePassenger.isOpened());
+
+        loginPagePassenger.inputUsername(PASSENGER_EMAIL);
+        loginPagePassenger.inputPassword(PASSENGER_PASSWORD);
+        loginPagePassenger.clickOnLogin();
+    }
+}
